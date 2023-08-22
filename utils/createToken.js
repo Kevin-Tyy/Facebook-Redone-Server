906fa3632e55ef4af5584ed59f626ae46217466f
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-08-22 23:51:22 +0200

    [bug] fixes

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,8 +1,8 @@
 const Jwt = require("jsonwebtoken");
 const jwtsecret = process.env.JWTSECRET;
 
-const createToken = async ({ firstname, lastname, userId, username }) => {
-	return Jwt.sign({ firstname, lastname, userId, username }, jwtsecret);
+const createToken = async ({ firstname, lastname, userId, username, profileimage }) => {
+	return Jwt.sign({ firstname, lastname, userId, username, profileimage }, jwtsecret);
 };
 
 module.exports = createToken;
